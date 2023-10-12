b443774d890fc7e12b7faa404729b52552d90156
Max Musing <max.musing@gmail.com>
2023-10-11 23:08:28 -0400

    Redirect to root on open

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const { menubar } = require("menubar");
 
-menubar({
+const baseUrl = "https://app.graphite.dev/";
+
+const mb = menubar({
   browserWindow: {
     width: 500,
     height: 800,
   },
   icon: "./icon.png",
-  index: "https://app.graphite.dev",
+  index: baseUrl,
+});
+
+mb.on("show", () => {
+  if (mb.window.webContents.getURL() !== baseUrl) {
+    mb.window.webContents.loadURL(baseUrl);
+  }
 });
